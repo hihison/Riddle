17715e218c23da3f152675869b0023533f501c7c
hihison <hihison@users.noreply.github.com>
2016-12-17 15:00:49 +0800

    ver 3.141
    Added random access the database
    Added pop out message boxes

--- riddle.cpp
+++ riddle.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <unistd.h>
 #include <time.h>
+#include <windows.h>
+
 
 void delay(int sec) 
 { 
@@ -13,6 +15,8 @@ void delay(int sec)
 	_t = time (NULL); // ?????? 
 	while(time (NULL) <= _t + sec){}; // ????? 
 } 
+//time delay for seconds
+
 int readfile (){
 	FILE *fPtr;
 	int read;
@@ -20,18 +24,19 @@ int readfile (){
     fscanf(fPtr, "%d", &read);
     fclose(fPtr);
      
-    printf("recent score : %d", read);
+    printf("Recent score : %d \n", read);
      
     return read;
 }
+//read file for user porfile
 
 void savefile (int score){
 	FILE *fPtr;
     fPtr = fopen("user_profile/data.txt", "w");
     fprintf(fPtr, "%d", score);
     fclose(fPtr);
-    //return 0;
 }
+//write file for user file
 
 void str_low(char *str1, char *strlow){
 int i=0,temp=0;
@@ -44,31 +49,15 @@ for (int j = 0  ; j <= strlen(str1)-1; j++ ){
 		}
 	}strlow[i]='\0';
 }
+//convert english characters to lowercase 
 
 
-int str_check(char *str1, char *strck){
-int i=0,wrong=0; int temp;
-for (int j = 0  ; j <= strlen(str1)-1; j++ ){
-	temp = strlen(str1) -1 ;
-	do{ 
-		if(str1[j]== strck[j]) wrong=0; else wrong=1;
-	} while ( j==strlen(str1) || wrong!=1 ) ;
-	}
-		if (wrong==1) {
-			for (int k = 0 ; k<= temp; k++) {
-				
-				strck[k] = '\0';
-			}
-		return 1;
-		
-		} 
-		else return 0;
-}
 
-	FILE *question;		FILE *ansfile;
-	char data[999], data2[999];
+
 
 int  main () {
+	FILE *question;		FILE *ansfile;
+	char data[999], data2[999];
 	char q[MAX][MAX] ;
 	char a[MAX][MAX] ;
 	char answer[MAX][MAX];
@@ -76,82 +65,83 @@ int  main () {
 	char choice;
 	int counter=0;
 	int score = readfile();
-		question = fopen("gamedata/question.txt", "r");
-		while (fgets(data,999,question)!=NULL){
-			strcpy(q[counter],data);
-			counter=counter+1;
-		} fclose(question); 
+	int rq ;
+	
+	question = fopen("gamedata/question.txt", "r");
+	while (fgets(data,999,question)!=NULL){
+		strcpy(q[counter],data);
+		counter=counter+1;
+	} fclose(question); 
 		
-		ansfile = fopen("gamedata/answer.txt", "r");
-
-		for (int get = 0; get<=counter ; get++){
-			data2[get] = fscanf(ansfile,"%s",a[get]);
-		}fclose(ansfile);
-
-	int count = 1;
-
-	for (int i = 0 ; i <= counter-1 ; i++ ){
-
-
+	ansfile = fopen("gamedata/answer.txt", "r");
+	for (int get = 0; get<=counter ; get++){
+		data2[get] = fscanf(ansfile,"%s",a[get]);
+	}fclose(ansfile);
+	//get questions and answer by reading txt files
+	
+	system("color F2");
+	system("@ECHO off");
+	int number ;
+	printf("Please type in how many questions you want to answer : ");
+	scanf("%d",&number);
+	//get times to answer
+	
+	for (int i = 0 ; i < number ; i++ ){
+	srand(time(NULL));
+	rq = rand()%(counter);
+	// Random access questions
 
 	do{
 		system("COLOR F0");
 		system("cls");
 	    printf("Recent score : %d \n", score);
-		printf("%s\n",q[i]);
-
+		printf("%s\n",q[rq]);
 		printf("Answer Here: ");
-	
 		scanf("%s",&answer[i]);
 		str_low(answer[i],anslow[i]);
-	
-	
 		printf("you typed : %s , Sure? (Y/N) \n",anslow[i]);
 		scanf("%s",&choice);
 		} while (choice!=89 && choice!=121);
-			int iscorrect = strcmp(anslow[i],a[i]);
+			int iscorrect = strcmp(anslow[i],a[rq]);
 		
 			if (iscorrect==0) {
 				score=score+5;
 				savefile (score);
+				MessageBox(NULL, "Oops! Correct!", "OMG", MB_OK);	
 				system("color 2F");	
 				printf("you are right!\n") ;
-				delay(0.2);
-	
+				delay(0.1);
 				system("color F2");	
 				printf("scores + 5\n") ;
-				delay(0.2);
-	
+				delay(0.1);
 				system("color 2F");	
-				
 				printf("You now have scores : %d  \n",score) ;
-				delay(0.2);
-	
+				delay(0.1);
 				system("color F2");	
 				printf("Congratulations !\n") ;
-				delay(1);
+				delay(0.5);
 			}
 			 else {
 			    score=score-5;
 			 	savefile (score);
-				system("COLOR 4F"); 	
-		    	printf("Wrong, the answer is %s\n",a[i]);
+				system("COLOR 4F"); 
+				MessageBox(NULL, "Oops! Wrong!", "OMG", MB_OK);	
+		    	printf("Wrong, the answer is %s\n",a[rq]);
  		    	delay(0.2);
-
-			    system("COLOR F4"); 	
+				system("COLOR F4"); 	
 			    printf("Unluckly , you lose $5\n");
  			    delay(0.2);		
-
 			    system("COLOR 4F"); 
 			    printf("You now have scores : %d  \n",score) ;
- 			    delay(1);	
+ 			    delay(0.5);	
 
 			}
 	}
+	savefile (score);
 	system("COLOR 61"); 	
 	system("cls");
-	printf("You totally have scores %d", score);
-	system("pause");
+	printf("You totally have scores %d\n", score);
+	MessageBox(NULL, "Good Bye!", "Bye", MB_OK);
 	
 	
 }
